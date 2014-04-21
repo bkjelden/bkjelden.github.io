ef4ebb1e6b0e8cdcd73c3f1a970865ccde9ef804
Billy Kjelden <billykjelden@gmail.com>
2014-04-21 17:33:55 -0500

    adding wordcloud.js

---
+++ facebook/wordcloud.js
@@ -0,0 +1,45 @@
+var WordCloud = WordCloud || (function(){}
+	var statuses = [];
+	var activeDelegates = [];
+	var wordsInCloud = [];
+	var setStatuses = function(_statuses){
+		statuses = _statuses;
+		updateCloud();
+	};
+	
+	var updateCloud = function(){
+		var wordCounts = {};
+		var wordCountsArray = [];
+		for(var i in statuses){
+			var passed = true;
+			for(var del in activeDelegates){
+				if(!activeDelegates[del](statuses[i]){ //delegate check failed
+					passed = false;
+					break;
+				}
+			}
+			if(passed){
+				var statusWords = statuses[i].message.match(/[a-z]{4,}/gim);
+				for(var j in statusWords){
+					var word = statusWords[j].toUpperCase();
+					if(wordCounts[word] === undefined){
+						wordCounts[word] = 1;	
+					}else{
+						wordCounts[word]++;
+					}
+				}
+			}
+		}
+		
+		for(var word in wordCounts){
+			wordCountsArray.push({ word: word, count: wordCounts[word] });
+		}
+		
+		wordCounts.sort(function(a,b){ return a.count - b.count; });
+		console.log(wordCounts);
+	};
+	
+	return {
+		setStatuses: setStatuses;
+	}
+)();
